3881c51f084debd83170d4adbfa3d10b7197e384
BibekPariyar <58519244+BibekPaiyar@users.noreply.github.com>
2020-11-05 14:25:08 +0545

    Update task#2.cpp

--- LAB 4/task#2.cpp
+++ LAB 4/task#2.cpp
@@ -18,6 +18,66 @@ public:
         m = (inch / 12 + feet) / 3.2808;
         return m;
     }
+    // WAP in CPP to convert the co-ordinates from Cartesian system to polar system using 
+// the concept of user defined to user defined conversion by writing conversion routine 
+// in source class.
+
+#include <iostream>
+#include <cmath>
+#define PI 3.1415
+using namespace std;
+
+
+class Polar{
+private:
+    float r, angle;
+public:
+    Polar(float r = 0, float angle = 0){
+        this->r = r;
+        this->angle = angle;
+    }
+    void showData(){
+        cout << "Polar co-ordinate: (" << r << ", " << angle << ") " << endl;
+    }
+
+};
+
+
+class Cartesian{
+private:
+    float x, y;
+
+public:
+    Cartesian(float x = 0, float y = 0){
+        this->x = x;
+        this->y = y;
+    }
+    operator Polar(){
+        float r, angle;
+        r = sqrt(pow(x,2) + pow(y,2));
+        angle = (180/PI)*atan(y/x);     // angle in degree
+        return Polar(r, angle);
+    }
+    void getData(){
+        cout << "Enter Cartesian co-ordinates : (x, y)" << endl;
+        cin >> x >> y;
+    }
+    void showData(){
+        cout << "Cartesian co-ordinate: (" << x << ", " << y << ") " << endl;
+    }
+
+};
+
+int main(){
+
+    Cartesian c;
+    c.getData();
+    Polar p;
+    p = c;
+    c.showData();
+    p.showData();
+    return 0;
+}
     void showData(){
         cout << "Distance in feet and inches is : "<< feet <<" feets, " << inch <<" inches."<<endl;
     }
@@ -25,7 +85,8 @@ public:
 
 int main(){
 
-    Distance d(5,4);
+    Distance d;
+    d.getData();
     d.showData();
     float m = d;
     cout << "Distance in meter : " << m << " meters." <<endl;
