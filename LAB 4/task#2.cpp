d1c35dc10a998dd46530771ee77eee57f2cb45b9
BibekPariyar <58519244+BibekPaiyar@users.noreply.github.com>
2020-11-05 14:25:45 +0545

    Update task#2.cpp

--- LAB 4/task#2.cpp
+++ LAB 4/task#2.cpp
@@ -1,4 +1,4 @@
-// Write a program in CPP to convert the distance in feet and inch entered by the user 
+// Write a program in CPP to convert the distance in feet and inch entered by the user
 //into distance in meters using the concept o user defined to basic data conversion.
 
 #include <iostream>
@@ -18,66 +18,10 @@ public:
         m = (inch / 12 + feet) / 3.2808;
         return m;
     }
-    // WAP in CPP to convert the co-ordinates from Cartesian system to polar system using 
-// the concept of user defined to user defined conversion by writing conversion routine 
-// in source class.
-
-#include <iostream>
-#include <cmath>
-#define PI 3.1415
-using namespace std;
-
-
-class Polar{
-private:
-    float r, angle;
-public:
-    Polar(float r = 0, float angle = 0){
-        this->r = r;
-        this->angle = angle;
-    }
-    void showData(){
-        cout << "Polar co-ordinate: (" << r << ", " << angle << ") " << endl;
-    }
-
-};
-
-
-class Cartesian{
-private:
-    float x, y;
-
-public:
-    Cartesian(float x = 0, float y = 0){
-        this->x = x;
-        this->y = y;
-    }
-    operator Polar(){
-        float r, angle;
-        r = sqrt(pow(x,2) + pow(y,2));
-        angle = (180/PI)*atan(y/x);     // angle in degree
-        return Polar(r, angle);
-    }
     void getData(){
-        cout << "Enter Cartesian co-ordinates : (x, y)" << endl;
-        cin >> x >> y;
+        cout << "Enter distance in feet and inch :" << endl;
+        cin >> feet >> inch ;
     }
-    void showData(){
-        cout << "Cartesian co-ordinate: (" << x << ", " << y << ") " << endl;
-    }
-
-};
-
-int main(){
-
-    Cartesian c;
-    c.getData();
-    Polar p;
-    p = c;
-    c.showData();
-    p.showData();
-    return 0;
-}
     void showData(){
         cout << "Distance in feet and inches is : "<< feet <<" feets, " << inch <<" inches."<<endl;
     }
